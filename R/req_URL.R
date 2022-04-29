ab182940026a9f48a71449562ffecc417453c7cf
LottePohl <lotte.pohl@imbrsea.eu>
2022-04-29 11:01:50 +0200

    document req_URL()

--- R/req_URL.R
+++ R/req_URL.R
@@ -1,13 +1,19 @@
-#' Create a base URL for a http request. Queries can be added to the URL.
+#' Create a base URL for a http request.
 #'
-#' @param api_type Type of API architecture. Must be either "rest" or "soap".
-#' @param file_format File format.
-#' @param method
+#' @param api_type Type of API architecture. Must be one of c("rest", "soap").
+#' @param file_format File format. Must be one of c("json", "xml", "ttl", "jsonld").
+#' @param method RESTful method. Check the Marineregions gazetteer at https://marineregions.org/gazetteer.php?p=webservices for available methods.
 #'
 #' @return A base URL to append queries to.
 #' @export
 #'
 #' @examples
+#' api <- "rest"
+#' file <- "json"
+#' method <- "getGazetteerRecordsByName"
+#' req_URL(api_type = api, file_format = file, method = method)
+#' # https://marineregions.org//rest/getGazetteerRecordsByName.json/
+
 req_URL <- function(api_type, file_format, method){
   URL <- "https://marineregions.org/"
   base_url <- glue::glue("{URL}/{api_type}/{method}.{file_format}/")
@@ -24,6 +30,5 @@ req_URL <- function(api_type, file_format, method){
   return(base_url)
 }
 
-# write function mr_gaz_methods() where you can see the available methods
+# write function mr_gaz_methods() where you can see the available methods & search for strings
 methods <- c("getGazetteerRecordByMRGID", "getGazetteerGeometry" , "getGazetteerTypes", "getGazetteerGeometries", "getGazetteerRecordsByName", "getGazetteerRecordsByType", "getGazetteerWMSes", "getGazetteerRecordsByLatLong", "getGazetteerRecordsByNames", "getGazetteerSources", "getGazetteerNamesByMRGID", "getGazetteerRecordsBySource", "getFeed", "getGazetteerRelationsByMRGID")
-
