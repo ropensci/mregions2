890763049e3ced9ec81e851509892634a5d35202
LottePohl <lotte.pohl@imbrsea.eu>
2022-05-02 16:25:39 +0200

    add test httr2 req to test-req_URL()
    to see if response status == 200

--- tests/testthat/test-req_URL.R
+++ tests/testthat/test-req_URL.R
@@ -1,7 +1,19 @@
 
-testthat::test_that("req_URL() provides a base URL for a http request", {
+testthat::test_that("req_URL() has valid input arguments", {
   test_url <- req_URL(api_type = "rest", file_format = "json", method = "getGazetteerRecordsByName")
   expect_url <- "https://marineregions.org//rest/getGazetteerRecordsByName.json/"
   expect_equal(test_url, expect_url)
-  # test if URL is a string or a URL
+
+
+# testthat "req_URL() is a valid URL"
+  test_name <- "High Seas"
+
+  test_req <- httr2::request(test_url) %>%
+    # httr2::req_headers(accept = "application/json") %>%
+    httr2::req_url_path_append(URLencode(test_name)) %>%
+    httr2::req_url_path_append("/")
+  test_resp <- test_req %>% httr2::req_perform()
+
+  status_ok <- 200
+  testthat::expect_equal(test_resp$status_code, status_ok)
 })
