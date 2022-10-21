2c7b0b67e6f8bd0dfdbbe29e77f84928a302d751
salvafern <salvador.fernandez@vliz.be>
2022-10-21 14:47:14 +0000

    add base graph

--- R/test_rdflib.R
+++ R/test_rdflib.R
@@ -1,38 +1,207 @@
 library(rdflib)
-library(httr)
+library(httr2)
 library(glue)
-mrgid <- 14
-url <- glue("http://marineregions.org/mrgid/{mrgid}")
+library(magrittr)
 
-# correct RFC
-# req <- GET("https://marineregions.org/mrgid/26567", accept("text/turtle"))
-req <- GET(url, accept("text/turtle"))
+# Check if cache dir exists and create if doesnt
+mr_cache <- function(){
 
-# req <- GET(url, accept("application/ld+json"))
+  path_cache <- file.path(Sys.getenv("MR_CACHE", unset = getwd()), "mr-cache/")
+  # path_graph <- file.path(path_cache, "mr_graph.rdf")
+  verbose <- getOption("verbose", default = TRUE)
 
-cont <- content(req, as = "text")
-cat(cont)
+  if(!dir.exists(path_cache)){
+    dir.create(path_cache)
 
-rdf <- rdflib::rdf_parse(cont, format = "turtle")
-# rdf <- rdflib::rdf_parse(cont, format = "jsonld")
+    if(verbose){
+      cli::cli_alert_success(glue::glue("Cache dir created in: `{path_cache}`"))
+    }
 
-# Get all prefixes
-todelete <- gsub("@prefix(.*?)\n", "", cont)
-prefix <- gsub(todelete, "", cont, fixed = TRUE)
-prefix <- gsub("@", "", prefix, fixed = TRUE)
-prefix <- gsub(" .", "", prefix, fixed = TRUE)
+  }
 
-cat(cont)
+  path_cache
+}
 
-## CREATE GENERAL FUNCTION TO GET MRGID AS RDF
+
+# Creates a basic request with general settings
+mr_request <- function(url, ...){
+  req <- httr2::request(url) %>%
+    httr2::req_user_agent("mregions2") %>%
+    httr2::req_cache(mr_cache())
+
+  # Add messages if verbose = TRUE
+  verbose <- getOption("verbose", default = FALSE)
+  if(verbose){
+    req <- req %>%
+      httr2::req_verbose(...) %>%
+      httr2::req_cache(mr_cache(), debug = TRUE)
+  }
+
+  req
+}
+
+# Performs the request
+mr_req_perform <- function(req, method = "GET"){
+  resp <- req %>%
+    httr2::req_perform(method)
+
+  verbose <- getOption("verbose", default = FALSE)
+  if(verbose){
+    resp %>%
+      httr2::resp_body_string() %>%
+      cat()
+  }
+
+  resp
+}
+
+# Parse as RDF
+mr_rdf_parse <- function(resp, ...){
+  rdf <- resp %>%
+    httr2::resp_body_string() %>%
+    rdflib::rdf_parse(...)
+
+  rdf
+}
+
+# Build base graph using known ontologies
+mr_graph_base_build <- function(){
+
+  # Cache control
+  mr_base_graph_path = file.path(mr_cache(), "mr_graph_base.rds")
+
+  if(file.exists(mr_base_graph_path)){
+    seconds_since_creation = as.numeric(difftime(Sys.time(), file.mtime(mr_base_graph_path), units = "secs"))
+    max_age = 604800
+
+    if(seconds_since_creation < max_age){
+      out <- readRDS(mr_base_graph_path)
+
+      verbose <- getOption("verbose", default = TRUE)
+      if(verbose){
+        cli::cli_alert_success(glue::glue("Cached base graph is fresh. Reading from `{mr_base_graph_path}`"))
+      }
+
+      # Premature end
+      return(out)
+
+    }else if(seconds_since_creation > max_age){
+      file.remove(seconds_since_creation)
+
+    }
+
+  }
+
+  # If not cached, then perform
+
+  # Get Turtle docs
+  uri_turtle <- list(
+    mr = "http://marineregions.org/ns/ontology",
+    mrt = "http://marineregions.org/ns/placetypes",
+    dcat = "https://www.w3.org/ns/dcat2",
+    prov = "http://www.w3.org/ns/prov"
+  )
+
+  reqs_turtle <- lapply(uri_turtle, mr_request) %>%
+    lapply(httr2::req_headers, accept = "text/turtle") %>%
+    httr2::multi_req_perform(cancel_on_error = F) %>%
+    lapply(mr_rdf_parse, format = "turtle")
+
+
+  # Get RDFXML docs
+  uri_rdfxml<- list(
+    skos = 'https://www.w3.org/2009/08/skos-reference/skos.rdf',
+    gsp = 'http://www.opengis.net/ont/geosparql'
+  )
+
+  reqs_rdfxml <- lapply(uri_rdfxml, mr_request) %>%
+    lapply(httr2::req_headers, accept = "application/xml+rdf") %>%
+    httr2::multi_req_perform(cancel_on_error = F) %>%
+    lapply(mr_rdf_parse, format = "rdfxml")
+
+
+  reqs <- c(reqs_turtle, reqs_rdfxml)
+
+  # Paste all documents together
+  out <- c(reqs[[1]])
+  for(i in 2:length(reqs)){
+    out <- c(out, reqs[[i]])
+  }
+
+  # Cache
+
+  saveRDS(out, file = mr_base_graph_path)
+
+  out
+
+}
+
+debug(mr_graph_base_build)
+base_graph <- mr_graph_base_build()
+
+readRDS("./mr-cache/mr_graph_base.rds")
+
+
+
+
+mr_base_graph_path = file.path(mr_cache(), "mr_base_graph.rds")
+
+if(file.exists(mr_base_graph_path)){
+  seconds_since_creation = as.numeric(difftime(Sys.time(), file.mtime(mr_base_graph_path), units = "seconds"))
+  max_age = 604800
+
+  if(seconds_since_creation < max_age){
+    out <- readRDS(mr_base_graph_path)
+
+  }else if(seconds_since_creation > max_age){
+    file.remove(seconds_since_creation)
+
+  }
+
+}
+
+
+saveRDS(out, file = )
+
+
+
+
+
+
+
+
+
+
+
+
+
+# debug(mr_get_rdf)
+options(verbose = TRUE)
+belgium <- mr_get_rdf("http://marineregions.org/mrgid/14")
+france <- mr_get_rdf("http://marineregions.org/mrgid/17")
+
+graph <- c(base_graph, belgium)
+
+
+
 ## CREATE GENERAL FUNCTION TO MERGE MRGIDS
 ## CREATE GENERAL FUNCTION TO APPLY SPARQL QUERY
 
 ## QUERIES
 ## GET ALL BASIC INFO
+
 query <- paste0(
-  prefix,
   "
+  prefix mr: <http://marineregions.org/ns/ontology#>
+  prefix mrt: <http://marineregions.org/ns/placetypes#>
+  prefix dc: <http://purl.org/dc/terms/>
+  prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+  prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+  prefix skos: <http://www.w3.org/2004/02/skos/core#>
+  prefix dcat: <http://www.w3.org/ns/dcat#>
+  prefix gsp: <http://www.opengis.net/ont/geosparql#>
+  prefix prov: <http://www.w3.org/ns/prov#>
+
   SELECT ?mrgid
          (COALESCE(?altLabel, ?prefLabel) AS ?label)
          ?placetype
@@ -51,31 +220,31 @@ query <- paste0(
              dcat:bbox ?bbox ;
              mr:hasGeometry ?the_geom .
 
+
+    # Get the language
     OPTIONAL {
-      ?mrgid skos:altLabel ?altLabel
+      ?mrgid skos:altLabel ?altLabel .
       filter langMatches(lang(?altLabel), \"nl\")
     }
-    OPTIONAL {
-      ?mrgid skos:prefLabel ?prefLabel
-    }
+    ?mrgid skos:prefLabel ?prefLabel .
 
     ?mrgid prov:hadPrimarySource ?primarySource .
     ?primarySource prov:wasAttributedTo ?wasAttributedTo .
     ?wasAttributedTo rdfs:label ?source .
 
+    # Approach using Property Paths, but seem like they are not supported
+    # ?mrgid prov:hadPrimarySource/prov:wasAttributedTo/rdfs:label ?source .
+
+   ?placetype skos:inScheme <http://marineregions.org/ns/placetypes> .
+
   }"
 )
-
+# ?placetype skos:inScheme <http://marineregions.org/ns/placetypes> .
 cat(query)
-out <- rdflib::rdf_query(rdf, query, data.frame = FALSE)
-out <- subset(out, out$placetype != 'http://marineregions.org/ns/ontology#MRGeoObject')
+out <- rdflib::rdf_query(graph, query, data.frame = FALSE)
+
+
 print(out, n = 100)
-
-
-
-
-
-
 
 #
 # "
@@ -95,12 +264,47 @@ print(out, n = 100)
 
 
 
-
-
-
-
 ## GET ALL RELATIONSHIPS
+query <- paste0(
+  prefix,
+  "
+  SELECT ?mrgid ?placetype ?prefLabel
+  WHERE {
+    ?mrgid a ?placetype ;
+          skos:prefLabel ?prefLabel .
+
+  }"
+)
+
+cat(query)
+out <- rdflib::rdf_query(rdf, query, data.frame = FALSE)
+out <- subset(out, out$placetype != 'http://marineregions.org/ns/ontology#MRGeoObject')
+out <- subset(out, out$mrgid != 'http://marineregions.org/mrgid/14')
+print(out, n = 100)
+
+query <- paste0(
+  prefix,
+  "
+  SELECT ?relationship ?value
+  WHERE {
+    <http://marineregions.org/mrgid/14> ?relationship ?value .
+  }"
+)
+
+relations <- rdflib::rdf_query(rdf, query, data.frame = FALSE)
+relations <- subset(relations, grepl("http://marineregions.org/ns/ontology#", relations$relationship, fixed = TRUE))
+
+out <- subset(out, out$placetype != 'http://marineregions.org/ns/ontology#MRGeoObject')
+out <- subset(out, out$mrgid != 'http://marineregions.org/mrgid/14')
+print(out, n = 100)
+
+
+
+
 ## GET GEOMETRY
+
+
+
 
 
 
@@ -175,6 +379,12 @@ rdflib::rdf_query(rdf, query, data.frame = FALSE)
 FILTER (regex(?predicate, '/mr:(\\w+)/g')) .
 
 
+# Set prefixes
+todelete <- gsub("@prefix(.*?)\n", "", resp)
+prefix <- gsub(todelete, "", resp, fixed = TRUE)
+prefix <- gsub("@", "", prefix, fixed = TRUE)
+prefix <- gsub(" .", "", prefix, fixed = TRUE)
+Sys.setenv(mr_prefix = prefix)
 
 
 
@@ -214,3 +424,15 @@ contains <- rdflib::rdf_query(rdf, query, data.frame = T)
 # How to select the skos:prefLabel from the relationships
 # How to get the placetype?
 # Create method to jump to next?
+
+
+"http://marineregions.org/ns/ontology" # no cacheable
+"http://marineregions.org/ns/placetypes.ttl" # no cacheable
+"https://www.w3.org/ns/dcat2" # cache-control + expires
+"http://www.w3.org/ns/prov" # cache-control + expires
+"http://www.w3.org/2000/01/rdf-schema" # cache-control + expires
+"http://www.opengis.net/ont/geosparql" # Last-Modified + ETag
+
+'http://purl.org/dc/terms/'
+"http://purl.org/dc/elements/1.1/"
+
